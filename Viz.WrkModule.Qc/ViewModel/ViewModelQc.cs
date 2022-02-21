3f91db4c8046a6047612ecdd0c5a901f1c47b1ca
Sobolev Mikhail <sobolev_mv@nlmk.com>
2022-02-21 12:08:33 +0500

    Выравнивание.

---
+++ Viz.WrkModule.Qc/ViewModel/ViewModelQc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using Smv.Print.RawData;
+using Smv.Utils;
+
+namespace Viz.WrkModule.Qc
+{
+  public class ViewModelQc
+  {
+    #region Fields
+    #endregion
+
+    #region Public Property
+    #endregion
+
+    #region Protected Method
+    #endregion
+
+    #region Private Method
+    #endregion
+
+    #region Constructor
+
+    public ViewModelQc(UserControl control, Object mainWindow)
+    {
+    }
+    #endregion
+
+    #region Command
+      #endregion
+
+
+    }
+}
